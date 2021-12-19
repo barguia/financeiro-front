5e3f5852b01ea95605aead28687dc7f0d58d16f2
Eduardo Barbelino da Purificação <eduardo.barbelino@gmail.com>
2021-12-19 10:22:02 -0300

    First commit

---
+++ src/components/AppGrid/AppGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <span :class="getClass()">
+    <slot/>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'AppGrid',
+  props: ['col', 'offset', 'size'],
+  methods: {
+    getClass() {
+      let classe = 'col';
+      classe += this.col ? `-${this.size}-${this.col}` : '';
+      classe = this.offset ? ` col-${this.size}-offset-md-${this.offset}` : '';
+      return classe;
+    }
+  }
+}
+</script>
--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-login">
-      Login
-    </button>
-
-    <modal identificador="modal-login" titulo="Login">
-      <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" class="form-control" name="email" id="email">
-      </div>
-      <div class="form-group">
-        <label for="email">Senha</label>
-        <input type="password" class="form-control" name="senha" id="senha">
-      </div>
-      <button class="btn btn-primary">Logar</button>
-    </modal>
+    <form @submit="logar()"></form>
+    <div class="form-group">
+      <label for="email">E-mail</label>
+      <input type="email" class="form-control" v-model="user.email">
+    </div>
+    <div class="form-group">
+      <label for="email">Senha</label>
+      <input type="password" class="form-control" v-model="user.password">
+    </div>
+    <button class="btn btn-primary" @click="logar()">Logar</button>
   </div>
 </template>
 
 <script>
-import Modal from '@/components/Modal'
+import axios from 'axios';
+
 export default {
   name: "Login",
-  components: {
-    Modal
+  data: {
+    user: {}
+  },
+  methods: {
+    logar() {
+
+    }
   }
 }
 </script>
